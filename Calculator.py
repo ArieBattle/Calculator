12005a5db73f0a63eee0043529add48d722adc2d
Arielle Battle <abattle@odin.cs.csubak.edu>
2021-04-29 14:59:20 -0700

    i did all the binary operations and loaded the matrix, fixed the menu

--- Calculator.py
+++ Calculator.py
@@ -7,16 +7,18 @@
 # Student 3: 
 # Description: Implementation of a scientific calculator ..
 
+import csv
+
 menu = {}
-menu['1']="ADD A and B: A + B"
-menu['2']="Subtract A from B: B - A"
-menu['3']="Subtract B from A: A - B"
-menu['4']="Multiply A and B: AB"
-menu['5']="Multiply B and A: BA"
-menu['6']="Copy A into B: A to B"
-menu['7']="Copy B into A: B to A"
-menu['8']="Swap A and B: A to B"
-menu['9']="Load matrix: Load A"
+menu['\n01']="ADD A and B: A + B"
+menu['02']="Subtract A from B: A - B"
+menu['03']="Subtract B from A: B - A"
+menu['04']="Multiply A and B: AB"
+menu['05']="Multiply B and A: BA"
+menu['06']="Copy A into B: A to B"
+menu['07']="Copy B into A: B to A"
+menu['08']="Swap A and B: A to B"
+menu['09']="Load matrix: Load A"
 menu['10']="Load matrix: Load B"
 menu['11']="Make A and square identify matrix up to oder 10x10: A to I"
 menu['12']="Make B and square identify matrix up to oder 10x10: B to I"
@@ -32,20 +34,172 @@ menu['21']="Integer power of A when A is a square matrix: A^n for 1 <= n <= 10"
 menu['22']="Integer power of B when B is a square matrix: B^n for 1 <= n <= 10"
 menu['23']="Exit"
 
+#function for add
+def add(A, B):
+    return A + B
+
+#function for subtract A - B
+def subtractAB(A, B):
+    return A - B
+
+#function for subtract B - A
+def subtractBA(A, B):
+    return B - A
+
+#function for multiply AB
+def multiplyAB(A, B):
+    return A * B
+
+#function for multiply BA
+def multiplyBA(A, B):
+    return B * A
+
+#function for copying A into B---------------------------
+#not sure if he wants us to keep the original number for B, same goes for the one below or use the copy funct for deep or shallow copy
+def copyAintoB(A, B):
+    temp = 0
+    temp = A
+    B = temp
+    return B
+
+#function for copying B int A---------------------------
+#same goes for this question
+def copyBintoA(A, B):
+    temp = 0
+    temp = B
+    A = temp
+    return A
+
+#function for making A and square identify matrix up to oder 10x10: A to I
+#function for making B and square identify matrix up to oder 10x10: A to I
+#function for scalar n times A
+#function for scalar n times B
+#function for det(A)
+#function for det(B)
+#function for transpose A
+#function for transpose B
+#function for inverse A
+#function for inverse B
+#function for Integer power of A when A is a square matrix
+#function for Integer power of B when B is a square matrix
+
+#while its not exit run menu program
 while True:
+
+    #get users input
     options=menu.keys()
     options.sort()
+
     for entry in options:
         print entry, menu[entry]
 
-    #has to be 'raw_input' in order to make selections 
-    selection = raw_input("Please select: ")
+    #has to be 'raw_input' in order to make selections for menu
+    selection = raw_input("\nEnter Choice: ")
     if selection =='1':
-        print("Input integer for A: ")
-        print("Input integer for A: ")
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print A, "+", B, "=", add(A, B)
+
+    elif selection == '2':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print A, ' - ', B, ' = ', subtractAB(A, B)
+
+    elif selection =='3':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print A, " - ", B, " = ", subtractBA(A, B)
+
+    elif selection == '4':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print B, " * ", A, " = ", multiplyAB(A, B)
+
+    elif selection =='5':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print B, " * ", A, " = ", multiplyBA(A, B)
+
+    elif selection == '6':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print "B = ", B, copyAintoB(A, B)
+
+    elif selection =='7':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print  copyBintoA(A, B),"A = ", A,
+
+    elif selection == '8':
+        A = float (input("Input integer for A: "))
+        B = float (input("input integer for B: "))
+        print "\nBefore swap: A = ", A, " B = ", B
+
+        temp = 0
+        temp2 = 0
+        temp = A
+        del A
+        temp2 = B
+        del B
+        A = temp2
+        B = temp
+
+        print "After Swapping: A = ", A, " B = ", B
+
+    elif selection =='9':
+        #this will open the file and read in the numbers
+        with open('A.csv', 'rb') as f:
+            reader = csv.reader(f)
+            data_as_list = list(reader)
+            print data_as_list
+
+    elif selection == '10':
+        #this will open the file and read in the numbers
+        B = float (input("input integer for B: "))
+        with open('B.csv', 'rb') as f:
+            reader = csv.reader(f)
+            data_as_list = list(reader)
+            print data_as_list
+
+    elif selection =='11':
+        A = float (input("Input integer for A: "))
+
+    elif selection == '12':
+        B = float (input("input integer for B: "))
+
+    elif selection =='13':
+        A = float (input("Input integer for A: "))
+
+    elif selection == '14':
+        B = float (input("input integer for B: "))
+
+    elif selection =='15':
+        A = float (input("Input integer for A: "))
+
+    elif selection == '16':
+        B = float (input("input integer for B: "))
+
+    elif selection =='17':
+        A = float (input("Input integer for A: "))
+
+    elif selection == '18':
+        B = float (input("input integer for B: "))
+
+    elif selection == '19':
+        A = float (input("Input integer for A: "))
+
+    elif selection =='20':
+        B = float (input("input integer for B: "))
+
+    elif selection == '21':
+        A = float (input("Input integer for A: "))
+
+    elif selection =='22':
+        B = float (input("input integer for B: "))
 
     elif selection == '23':
-        print("\nBYE")
+        print("\nExiting program")
         break
+
     else:
         print("\nMust be a number on menu!\n")
