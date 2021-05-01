f1459bc93e457f686356fe7d2fb54c2ef00c7121
Isaiah Medina <53984739+medinaisaiah2@users.noreply.github.com>
2021-04-30 18:20:52 -0700

    Just added my name

--- Calculator.rb
+++ Calculator.rb
@@ -3,6 +3,6 @@
 # Ruby IMPLEMENTATION OF A CUSTOM MATRIX CALCULATOR
 # date: 05/14/2021
 # Student 1: Arielle Battle
-# Student 2: 
+# Student 2: Javier I. Medina
 # Student 3: 
 # Description: Implementation of a scientific calculator ..
