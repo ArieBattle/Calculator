c96ad1272f9cfe1e01e7bd4cd6257283c835a648
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2021-04-30 19:25:55 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,7 @@
 # PYTHON IMPLEMENTATION OF A CUSTOM MATRIX CALCULATOR
 # date: 05/14/2021
 # Student 1: Arielle Battle
-# Student 2: Javier I Medina
+# Student 2: 
 # Student 3: 
 # Description: Implementation of a scientific calculator ..
 
